e9e4ae63763a127eb8ab0a1ea15f2211685ccbdd
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-07-10 17:22:06 +0530

    queries part done

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 import {ApolloServer,gql} from 'apollo-server';
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
+import { quotes, users } from './fakedb.js';
 
 
 const typeDefs = gql`
 type Query {
-    greet : String
-}`
-
+    users : [User]
+    user(id:ID!) : User
+    quotes : [Quote]
+    iquote(by:ID!) : [Quote]
+}
+type User{
+    id: ID!
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    quotes: [Quote]
+}
+type Quote{
+    name: String!
+    by: ID
+}
+`
 
 const resolvers ={
     Query:{
-        greet : ()=> "HEllo World"
+        users: ()=> users,
+        user: (parent,args)=> users.find((user)=> user.id==args.id),
+        quotes: ()=> quotes,
+        iquote: (parent,args)=> quotes.filter((quote)=> quote.by==args.by), 
+    },
+    User:{
+        quotes: (user)=> quotes.filter((quote)=> quote.by == user.id)
     }
 }
 
@@ -18,10 +40,9 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
     plugins:[
-        
+        ApolloServerPluginLandingPageGraphQLPlayground
     ]
   });
-
 
   server.listen('4000').then(({url})=>{
     console.log(`🚀 Server ready at ${url}`)
